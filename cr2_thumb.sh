421157d21776a34df33af0649dc66b63de6d3fef
Carno <carnophage@dobramama.pl>
2011-11-05 19:13:09 +0100

    Added cr2_thumb.sh script

---
+++ cr2_thumb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Simple script to extract jpg thumbnails from Canon's CR2 files
+
+DIR=$(pwd)
+
+# Let's create a folder for the extracted files
+if [ ! -d ${DIR}/thumb ]; then
+	echo " [info] $DIR/thumb doesn't exist, creating a new one..."
+	mkdir ${DIR}/thumb
+else
+	echo " [info] Folder ${DIR}/thumb already exists"
+fi
+
+# Let's extract the thumbnails and move them to the thumb folder
+for i in $(find ${DIR} -type f -name "*.CR2"); do
+	echo " [info] Working on: $(basename ${i})"
+	dcraw -e "${i}"
+	mv ${DIR}/$(basename ${i} .CR2).thumb.jpg ${DIR}/thumb/
+done
+
+exit 0
