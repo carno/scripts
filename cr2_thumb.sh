1df55846cc3dea078e3d6a7f587e2a7f317ce8fe
Carno <carnophage@dobramama.pl>
2012-09-01 21:53:42 +0200

    :retab

--- cr2_thumb.sh
+++ cr2_thumb.sh
@@ -6,17 +6,17 @@ DIR=$(pwd)
 
 # Let's create a folder for the extracted files
 if [ ! -d ${DIR}/thumb ]; then
-	echo " [info] $DIR/thumb doesn't exist, creating a new one..."
-	mkdir ${DIR}/thumb
+    echo " [info] $DIR/thumb doesn't exist, creating a new one..."
+    mkdir -p ${DIR}/thumb
 else
-	echo " [info] Folder ${DIR}/thumb already exists"
+    echo " [info] Folder ${DIR}/thumb already exists"
 fi
 
 # Let's extract the thumbnails and move them to the thumb folder
 for i in $(find ${DIR} -type f -name "*.CR2"); do
-	echo " [info] Working on: $(basename ${i})"
-	dcraw -e "${i}"
-	mv ${DIR}/$(basename ${i} .CR2).thumb.jpg ${DIR}/thumb/
+    echo " [info] Working on: $(basename ${i})"
+    dcraw -e "${i}"
+    mv ${DIR}/$(basename ${i} .CR2).thumb.jpg ${DIR}/thumb/
 done
 
 exit 0
