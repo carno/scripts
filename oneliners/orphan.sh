dbe0a68ae82900ae544f66121571e6cb268fe9f0
Carno <carnophage@dobramama.pl>
2020-02-16 22:47:28 +0100

    Check orphans without apt-show-versions

--- oneliners/orphan.sh
+++ oneliners/orphan.sh
@@ -3,4 +3,6 @@
 # List packages that are not available in currently enabled repositories
 # Note: packages installed manually with dpkg -i will also be listed
 
-apt-show-versions | awk '/No available version in archive/{print $1}'
+# apt-show-versions | awk '/No available version in archive/{print $1}'
+
+apt list --installed | awk -F/ '/\[installed,local\]/{print $1}'
