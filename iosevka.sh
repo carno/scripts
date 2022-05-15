c6c29de12905c0904fdf008bff657971f983b7bc
Carno <carnophage@dobramama.pl>
2022-05-15 23:09:27 +0200

    feat(iosevka): Patch with nerdfonts

--- iosevka.sh
+++ iosevka.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 declare -r _VERSION="${1:?Missing Iosevka version}"
-declare -a _FILES=("super-ttc-iosevka-")
+declare -a _FILES=("ttf-iosevka-term-")
 _TMP=$(mktemp -d)
 declare -r _TMP
 
@@ -18,11 +18,27 @@ for prefix in "${_FILES[@]}"; do
     curl -sSLO --fail --output-dir "${_TMP}" "https://github.com/be5invis/Iosevka/releases/download/v${_VERSION}/${prefix}${_VERSION}.zip"
 done
 
+echo "Extracting archives…"
+unzip -o -u "${_TMP}/*.zip" "iosevka-term-medium*.ttf" -d "${_TMP}/in"
+mkdir -p "${_TMP}/out"
+
+echo "Patching with nerdfonts…"
+set +e
+docker run \
+    --user "$(id -u):$(id -g)" \
+    --volume "${_TMP}/in":/in \
+    --volume "${_TMP}/out":/out \
+    --rm \
+    nerdfonts/patcher --complete --careful --no-progressbars --quiet
+set -e
+
 echo "Removing old fonts…"
+# shellcheck disable=SC2086
 rm -rf ${HOME}/.fonts/[iI]osevka*
 
-echo "Extracting archives…"
-unzip -o -u "${_TMP}/*.zip" -d "${HOME}/.fonts"
+echo "Copying patched fonts…"
+# shellcheck disable=SC2086
+cp -v ${_TMP}/out/* "${HOME}/.fonts/"
 
-# update font cache
+echo "Refreshing font cache…"
 fc-cache -f -v
